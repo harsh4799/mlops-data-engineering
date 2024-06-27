6fd0ecac991fc9ca1cc95e49e539534b6f33ab64
Kunal18 <kunalindore18@gmail.com>
2024-06-27 15:12:37 -0400

    Final Code changes and clean up

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from .fraud_detection_dag import *
--- dags/fraud_detection_dag.py
+++ dags/fraud_detection_dag.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
-from operators import perform_eda,extract_data, load_to_s3, check_anomalies, visualize_categorical_columns, visualize_distance_from_home, visualize_distance_from_last_transaction, visualize_ratio_to_median_purchase_price
+from operators import perform_eda,extract_data, load_to_s3, check_anomalies, visualize_categorical_columns, visualize_class_distribution, visualize_distance_from_home, visualize_distance_from_last_transaction, visualize_ratio_to_median_purchase_price
 from datetime import timedelta
 
 default_args = {
@@ -50,6 +50,14 @@ visualize_ratio_to_median_purchase_price_task = PythonOperator(
 )
 
 
+visualize_distance_from_home_task = PythonOperator(
+    task_id='visualize_distance_from_home',
+    python_callable=visualize_distance_from_home,
+    provide_context=True,
+    dag=dag,
+)
+
+
 visualize_distance_from_last_transaction_task = PythonOperator(
     task_id='visualize_distance_from_last_transaction',
     python_callable=visualize_distance_from_last_transaction,
@@ -57,14 +65,14 @@ visualize_distance_from_last_transaction_task = PythonOperator(
     dag=dag,
 )
 
-
-
-visualize_distance_from_home_task = PythonOperator(
-    task_id='visualize_distance_from_home',
-    python_callable=visualize_distance_from_home,
+visualize_class_distribution_task = PythonOperator(
+    task_id='visualize_class_distribution',
+    python_callable=visualize_class_distribution,
     provide_context=True,
     dag=dag,
 )
+
+
 
 check_anomalies_task = PythonOperator(
     task_id='check_anomalies',
@@ -79,17 +87,22 @@ load_task = PythonOperator(
     dag=dag,
 )
 
+
 (
     extract_task
     >> check_anomalies_task
     >> [
-        eda_task,
         visualize_categorical_columns_task,
+        visualize_class_distribution_task,
+    ]
+    >>  eda_task
+    >> [
         visualize_distance_from_home_task,
         visualize_distance_from_last_transaction_task,
         visualize_ratio_to_median_purchase_price_task,
     ]
     >> load_task
 )
+
 # extract_task >> [eda_task, visualization_task] >> load_task
 
